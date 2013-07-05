174609ac250d688f133cba683bd675a40149f5e2
batitous <bburles@gmail.com>
2013-07-05 10:53:30 +0200

    Add Xcode project

---
+++ NativeServer/sources/main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  NativeServer
+//
+//  Created by bburles on 05/07/13.
+//  Copyright (c) 2013 Dviance. All rights reserved.
+//
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    
+    
+    printf("NativeServer Hello World\n");
+    
+    
+    
+    
+    
+    
+    return 0;
+}
