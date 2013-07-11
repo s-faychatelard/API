bd4987741ec03f37f94a3ed86e9f96045d5b9737
Sylvain Fay-Chatelard <sylvain.faychatelard@gmail.com>
2013-07-11 15:33:56 +0200

    Zmq

--- CAwaPI/sources/zmqnetworkclient.cpp
+++ CAwaPI/sources/zmqnetworkclient.cpp
@@ -1,51 +1,3 @@
-/*package com.awabot.AwaPI.network.client;
-
-import org.zeromq.ZMQ;
-import org.zeromq.ZMQ.Socket;
-import org.zeromq.ZContext;
-
-public class ZmqNetworkClient extends NetworkClient {
-	
-	private final ZContext	zContext;
-	private Socket 		zSocket;
-	
-	public ZmqNetworkClient()
-	{
-		super();
-		
-		zContext = new ZContext();
-	}
-	
-	public boolean open(String address)
-	{
-		zSocket = zContext.createSocket(ZMQ.REQ);
-		zSocket.connect(address);
-		
-		return true;
-	}
-	
-	public void close()
-	{
-		zContext.close();
-	}
-	
-	public void writeBuffer()
-	{
-		byte []buffer = writeStream.toByteArray();
-		
-		zSocket.send(buffer);
-		writeStream.reset();
-		
-		buffer = zSocket.recv();
-		
-		System.out.println("Read : " + buffer.length);
-		
-		readStream.reset();
-		readStream.writeNBytes(buffer, buffer.length);
-		readStream.reset();
-	}
-}*/
-
 #include "../includes/ZmqNetworkClient.h"
 
 ZmqNetworkClient::ZmqNetworkClient() : NetworkClient()
@@ -70,5 +22,9 @@ void ZmqNetworkClient::close()
 void ZmqNetworkClient::writeBuffer()
 {
     zmq_send (zSocket, writeStream->buffer, getByteStreamSize(writeStream), 0);
-    zmq_recv(zSocket, buffer, 10, 0);
+    resetByteStream(writeStream);
+    
+    resetByteStream(readStream);
+    zmq_recv(zSocket, readStream->buffer, getByteStreamSize(readStream), 0);
+    resetByteStream(readStream);
 }
